c9f1a9e702a7501e2306c8bd44acf950045246a9
Kat Reinhart <kat.reinhart@gmail.com>
2017-10-27 15:29:45 -0700

    Add David S. Pumpkins

--- scripts/main.js
+++ scripts/main.js
@@ -4,9 +4,9 @@ $('#spooky-button-1').click((e) => {
 })
 
 $('#spooky-button-2').click((e) => {
-
+  $('.container').append('<img src="/assets/img/dsp.png" width=300>')  
 })
 
-$('#spooky-button-2').click((e) => {
+// $('#spooky-button-2').click((e) => {
   
-})
+// })
